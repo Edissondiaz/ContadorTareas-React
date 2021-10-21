e2d8a3d53c25c77d47ffadb9b5802758097ad6fa
Edissondiaz <edissondiaz123@gmail.com>
2021-10-21 00:12:46 +0000

    guardado

---
+++ src/js/component/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Todo } from "./todo";
+
+export function Home() {
+	return (
+		<div className="text center mt-5">
+			<h1>Tareas !</h1>
+			<div className="row">
+				<div className="col-md-3"></div>
+				<div className="col-md-6">
+					<Todo />
+				</div>
+				<div className="col-md-3"></div>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/todo.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+export const Todo = () => {
+	let [counter, setCounter] = useState(0);
+	const [tasks, setTasks] = useState("");
+	const [list, setList] = useState([]);
+
+	const deleteTask = indexDelete => {
+		let resultado = list.filter((tasks, index) => index != indexDelete);
+		setList(resultado);
+		setCounter(counter - 1);
+	};
+
+	function agregar(e) {
+		setTasks(e);
+	}
+	const agregarLista = e => {
+		if (tasks != "") {
+			if (e.key == "Enter") {
+				setCounter(counter + 1);
+				setList([...list, tasks]);
+				setTasks("");
+			}
+		}
+	};
+
+	return (
+		<div>
+			<ul className="list-group">
+				<li className="list-group-item">
+					<input
+						type="text"
+						className="form-control"
+						id="todo"
+						placeholder="Agregar Tarea?"
+						onChange={e => agregar(e.target.value)}
+						onKeyPress={agregarLista}
+						value={tasks}></input>
+				</li>
+				{list.map((item, index) => {
+					return (
+						<li
+							key={index}
+							className="list-group-item"
+							id="distintor">
+							{item}
+							<i
+								className="fas fa-times"
+								onClick={() => {
+									deleteTask(index);
+								}}></i>
+						</li>
+					);
+				})}
+				<li className="list-group-item">
+					<small>
+						{"" +
+							(counter == 0
+								? "sin tareas, Agregar tarea"
+								: counter + " Tarea agregada")}
+					</small>
+				</li>
+			</ul>
+		</div>
+	);
+};
